6e31c2dea1374e88fb5be5292786695c12e9552d
Adrian Heaney <adrn@hotmail.co.uk>
2021-07-15 20:36:04 +0100

    add logging for datadog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   def endpoint_1
     # call service_1
     res = Faraday.get service_1_url, s1: params['s1']
-    Rails.logger.info("service_1 status: #{res.status}")
+    Rails.logger.info({ message: "service_1 status: #{res.status}" }.as_json)
 
     # call service_2
     res = Faraday.get service_2_url, s2: params['s2']
-    Rails.logger.info("service_2 status: #{res.status}")
+    Rails.logger.info({ message: "service_2 status: #{res.status}" }.as_json)
 
     # return
     render html: nil, status: :ok
